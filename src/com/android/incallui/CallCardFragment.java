19a1cd6c258e65445fc917194a8c3353d8ba9bc1
Wei Huang <wei.x.huang@sonymobile.com>
2015-02-10 13:46:41 +0800

    CallCardFragment: Fix OOM issue
    Signed-off-by: Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>

--- src/com/android/incallui/CallCardFragment.java
+++ src/com/android/incallui/CallCardFragment.java
@@ -1306,6 +1306,9 @@ public class CallCardFragment extends BaseFragment<CallCardPresenter, CallCardPr
                         setViewStatePostAnimation(listener);
                         mIsAnimating = false;
                         InCallPresenter.getInstance().onShrinkAnimationComplete();
+                        if (animator != null) {
+                            animator.removeListener(this);
+                        }
                     }
                 });
                 animator.start();
